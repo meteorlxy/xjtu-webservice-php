e68d098f6921e860dd0b3fedaca373a21c619253
meteorlxy <meteor.lxy@foxmail.com>
2017-12-02 10:56:38 +0000

    refactor: refactor the code, add options for SoapClient

--- tests/index.php
+++ tests/index.php
@@ -3,14 +3,30 @@
 require '../vendor/autoload.php';
 
 use Xjtuana\XjtuWs\WebService\WsUserInfo;
+use Xjtuana\XjtuWs\WebService\WsUserPhoto;
+use Xjtuana\XjtuWs\WebService\WsSms;
 use Xjtuana\XjtuWs\WebService\XjtuWebServiceException;
 
 $config = require './config.php';
 try {
 
-    $userinfo = new WsUserInfo($config['userinfo']);
+    $userinfo = new WsUserInfo($config['userinfo']['url'], $config['userinfo']['config'], [
+        'connection_timeout' => 10,
+    ]);
     
-    $result = $userinfo->getByNetid('netid');
+    $res_userinfo = $userinfo->getByNetid('netid');
+
+    $userphoto = new WsUserPhoto($config['userphoto']['url'], $config['userphoto']['config'], [
+        'connection_timeout' => 10,
+    ]);
+    
+    $res_userphoto = $userphoto->getByUserno('userno');
+
+    $sms = new WsSms($config['sms']['url'], $config['sms']['config'], [
+        'connection_timeout' => 10,
+    ]);
+    
+    $res_sms = $sms->send('mobile_number', 'sms_content');
     
 } catch(XjtuWebServiceException $e) {
     echo $e->getMessage();
@@ -18,4 +34,8 @@ try {
     throw $e;
 }
 
-var_dump($result);
+var_dump($res_userinfo);
+echo '<br>';
+var_dump($res_userphoto);
+echo '<br>';
+var_dump($res_sms);
